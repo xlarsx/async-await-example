964d28e5bfe5366176651b8709b86f3ceedf6c8b
Luis Rangel <luis.sanchez@liquidlabs.de>
2016-02-05 17:02:46 +0100

    Using twilio promises to use with async

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 
 http://stackoverflow.com/questions/33527653/babel-6-regeneratorruntime-is-not-defined-with-async-await?answertab=active#tab-top
+https://www.twilio.com/blog/2013/11/using-promises-in-the-twilio-module-for-node-js.html
--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 "use strict";
 
+var twilio = require('twilio');
 
-async function foo () {
-  var s = await bar();
-  console.log(s);
+const AccountSid = '[AccountSid]';
+const AuthToken = '[AuthToken]';
+const TO_PHONE_NUMBER = '[TO_PHONE_NUMBER]';
+const FROM_PHONE_NUMBER = '[FROM_PHONE_NUMBER]';
+
+var client = twilio(AccountSid, AuthToken);
+
+async function sendMessage (to) {
+  try {
+    await client.sendMessage({
+      to   : to,
+      from : FROM_PHONE_NUMBER,
+      body : 'Hello, there'
+    });
+    console.log('Message sent');
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function bar () {
-  return "bar";
-}
-
-foo();
+sendMessage(TO_PHONE_NUMBER);
